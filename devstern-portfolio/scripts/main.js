ab91ee3785a604a6df8462f9b630be48338c1ff1
hussainshamim16 <hj422748@gmail.com>
2025-08-25 13:41:59 +0500

    devstren portfolio sections ready

---
+++ devstern-portfolio/scripts/main.js
@@ -0,0 +1,6 @@
+AOS.init({
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 300, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+});
